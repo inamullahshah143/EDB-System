c6ca19e88ec3b80cd87b5c8e506d16dbbfe22d93
Inamullah Shah <89691107+inamullahshah143@users.noreply.github.com>
2023-03-15 15:45:34 +0500

    .

--- edb_management_system/vendor/composer/installed.php
+++ edb_management_system/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'eaec6020b58c9342c9dcbb99eee0b5afacd9afb4',
+        'reference' => 'af0eaee01f8702800277a30ff3925304c6fbf97b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -424,7 +424,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'eaec6020b58c9342c9dcbb99eee0b5afacd9afb4',
+            'reference' => 'af0eaee01f8702800277a30ff3925304c6fbf97b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -820,8 +820,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0.0 || 2.0.0 || 3.0.0',
-                1 => '1.0|2.0',
+                0 => '1.0|2.0',
+                1 => '1.0.0 || 2.0.0 || 3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
